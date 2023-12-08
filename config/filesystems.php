962b60c4e7789e6b568acad6b74a7a527cc58208
Reynoldssss <michaelreynoldk@gmail.com>
2023-12-08 23:39:01 +0700

    changin public path

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/storage/public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -77,7 +77,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/storage/public'),
     ],
 
 ];
